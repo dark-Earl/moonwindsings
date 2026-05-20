91f710816905767195754ea992c3c6e9b15c8890
QiuMingjie <2980586297@qq.com>
2026-05-20 17:00:51 +0800

    update: 部分问题调整

--- src/pages/imagePresenter/router.js
+++ src/pages/imagePresenter/router.js
@@ -25,11 +25,6 @@ router.addRoutes([
     path: '/imgShowMineAI',
     meta: { title: '图片展示', icon: '', needLogin: false},
     component: () => import('@/pages/imagePresenter/views/imgShowMineAI.vue')
-  },
-  {
-    path: '/imgShowPicDemo1',
-    meta: { title: '图片展示', icon: '', needLogin: false},
-    component: () => import('@/pages/imagePresenter/views/imgShowPicDemo1.vue')
-  },
+  }
 ])
 export default router
--- vue.config.js
+++ vue.config.js
@@ -56,7 +56,7 @@ const Timestamp = new Date().getTime()
 
 module.exports = {
   publicPath: process.env.BASE_URL, // 部署应用包时的基本 URL
-  outputDir: 'dist', // build 时生成的生产环境构建文件的目录
+  outputDir: 'docs', // build 时生成的生产环境构建文件的目录
   assetsDir: 'static', // 放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录
   productionSourceMap: false,
   transpileDependencies: [
